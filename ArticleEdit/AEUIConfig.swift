89392f97f32613f3aecb0e6eae80424d9f5e4750
Garen <915574843@qq.com>
2017-09-03 11:09:25 +0800

    change AEUIConfig to public

--- ArticleEdit/AEUIConfig.swift
+++ ArticleEdit/AEUIConfig.swift
@@ -9,9 +9,9 @@
 import Foundation
 import UIKit
 
-class AEUIConfig: NSObject {
+public class AEUIConfig: NSObject {
 	
-	static let shared = AEUIConfig()
+	public static let shared = AEUIConfig()
 	
 	private override init() {
 		super.init()
@@ -30,84 +30,84 @@ class AEUIConfig: NSObject {
 		return image
 	}
 	
-	class TextContentConfig {
-		var placeholder: String? = "请输入招商内容"
+	public class TextContentConfig {
+		public var placeholder: String? = "请输入招商内容"
 		
-		var placeholderColor: UIColor = .lightGray
+		public var placeholderColor: UIColor = .lightGray
 		
-		var font: UIFont = UIFont.systemFont(ofSize: 16)
+		public var font: UIFont = UIFont.systemFont(ofSize: 16)
 		
-		var textColor: UIColor = .black
+		public var textColor: UIColor = .black
 		
-		var leastTextHeight: CGFloat = 20
+		public var leastTextHeight: CGFloat = 20
 		
-		var insets: UIEdgeInsets = UIEdgeInsets(top: 8, left: 15, bottom: 8, right: 15)
+		public var insets: UIEdgeInsets = UIEdgeInsets(top: 8, left: 15, bottom: 8, right: 15)
 	}
 	
-	class SeparatorConfig {
-		var height: CGFloat = 1 / UIScreen.main.scale
+	public class SeparatorConfig {
+		public var height: CGFloat = 1 / UIScreen.main.scale
 		
-		var color: UIColor = .lightGray
+		public var color: UIColor = .lightGray
 		
-		var insets: UIEdgeInsets = UIEdgeInsets(top: 4, left: 15, bottom: 4, right: 15)
+		public var insets: UIEdgeInsets = UIEdgeInsets(top: 4, left: 15, bottom: 4, right: 15)
 	}
 	
-	class HeadImageContentConfig {
+	public class HeadImageContentConfig {
 		
-		var height: CGFloat = UIScreen.main.bounds.size.width / 3
+		public var height: CGFloat = UIScreen.main.bounds.size.width / 3
 		
-        var headImagePlaceholder: UIImage? = AEUIConfig.image(name: "icon_headImage_loading.png")
+        public var headImagePlaceholder: UIImage? = AEUIConfig.image(name: "icon_headImage_loading.png")
         
-		var addedButtonTitle: String? = "添加宣传图"
-		var addedButtonTitleFont: UIFont = UIFont.systemFont(ofSize: 16)
-		var addedButtonTitleColor: UIColor = UIColor.gray
-		var addedButtonImage: UIImage? = AEUIConfig.image(name: "icon_addimg99.png")
+		public var addedButtonTitle: String? = "添加宣传图"
+		public var addedButtonTitleFont: UIFont = UIFont.systemFont(ofSize: 16)
+		public var addedButtonTitleColor: UIColor = UIColor.gray
+		public var addedButtonImage: UIImage? = AEUIConfig.image(name: "icon_addimg99.png")
 	
 		
-		var changeButtonTitle: String? = "选择宣传图"
-		var changeButtonTitleFont: UIFont = UIFont.systemFont(ofSize: 14)
-		var changeButtonTitleColor: UIColor = UIColor.white
-		var changeButtonImage: UIImage? = AEUIConfig.image(name: "icon_changeimg.png")
+		public var changeButtonTitle: String? = "选择宣传图"
+		public var changeButtonTitleFont: UIFont = UIFont.systemFont(ofSize: 14)
+		public var changeButtonTitleColor: UIColor = UIColor.white
+		public var changeButtonImage: UIImage? = AEUIConfig.image(name: "icon_changeimg.png")
 
-		var changeButtonSize: CGSize = CGSize(width:128, height: 44)
+		public var changeButtonSize: CGSize = CGSize(width:128, height: 44)
 		
-		var bottomSeparatorColor: UIColor = UIColor(red: 0.94, green: 0.94, blue: 0.94, alpha: 1.0)
+		public var bottomSeparatorColor: UIColor = UIColor(red: 0.94, green: 0.94, blue: 0.94, alpha: 1.0)
 		
-		var bottomSeparatorHeight: CGFloat = 4
+		public var bottomSeparatorHeight: CGFloat = 4
 		
-		var insets: UIEdgeInsets = UIEdgeInsets(top: 13, left: 15, bottom: 13, right: 15)
+		public var insets: UIEdgeInsets = UIEdgeInsets(top: 13, left: 15, bottom: 13, right: 15)
 	}
 	
-	class ImageContentConfig {
-		var minimumsHeight: CGFloat = 100
+	public class ImageContentConfig {
+		public var minimumsHeight: CGFloat = 100
 		
-        var imageContentPlaceholder: UIImage? = AEUIConfig.image(name: "icon_imageContent_loading.png")
+        public var imageContentPlaceholder: UIImage? = AEUIConfig.image(name: "icon_imageContent_loading.png")
         
-		var deletedButtonImage: UIImage? = AEUIConfig.image(name: "btn_del.png")
+		public var deletedButtonImage: UIImage? = AEUIConfig.image(name: "btn_del.png")
 		
-		var editingViewBackgroundColor: UIColor = UIColor(colorLiteralRed: 0, green: 0, blue: 0, alpha: 0.5)
+		public var editingViewBackgroundColor: UIColor = UIColor(colorLiteralRed: 0, green: 0, blue: 0, alpha: 0.5)
 		
-		var editingContentTextFont: UIFont = UIFont.systemFont(ofSize: 14)
+		public var editingContentTextFont: UIFont = UIFont.systemFont(ofSize: 14)
 		
-		var editingContentTextColor: UIColor = .white
+		public var editingContentTextColor: UIColor = .white
 		
-		var editingButtonImage: UIImage? = AEUIConfig.image(name: "icon_editor.png")
+		public var editingButtonImage: UIImage? = AEUIConfig.image(name: "icon_editor.png")
 		
-		var editingButtonTextFont: UIFont = UIFont.systemFont(ofSize: 14)
+		public var editingButtonTextFont: UIFont = UIFont.systemFont(ofSize: 14)
 		
-		var editingButtonText: String? = "编辑描述"
+		public var editingButtonText: String? = "编辑描述"
 		
-		var insets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+		public var insets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
 	}
 	
-	class TitleConfig {
-		var placeholder: String? = "请输入招商标题"
+	public class TitleConfig {
+		public var placeholder: String? = "请输入招商标题"
 		
-		var placeholderColor: UIColor = .lightGray
+		public var placeholderColor: UIColor = .lightGray
 		
-		var maxTextCount: Int = 20
+		public var maxTextCount: Int = 20
 		
-		var titleFont: UIFont = {
+		public var titleFont: UIFont = {
 			if #available(iOS 8.2, *) {
 				return UIFont.systemFont(ofSize: 16, weight: UIFontWeightMedium)
 			} else {
@@ -115,28 +115,28 @@ class AEUIConfig: NSObject {
 			}
 		}()
 		
-		var titleColor: UIColor = .black
+		public var titleColor: UIColor = .black
 		
-		var leastTextHeight: CGFloat = 20
+		public var leastTextHeight: CGFloat = 20
 		
-		var insets: UIEdgeInsets = UIEdgeInsets(top: 14, left: 15, bottom: 14, right: 15)
+		public var insets: UIEdgeInsets = UIEdgeInsets(top: 14, left: 15, bottom: 14, right: 15)
 	}
 	
-	class PosterConfig {
-		var avaterHeight: CGFloat = 44
-        var avatarPlaceholder: UIImage? = AEUIConfig.image(name: "user.png")
-		var nameColor: UIColor = UIColor.darkGray
-		var nameFont: UIFont = UIFont.systemFont(ofSize: 15)
-		var timeTextColot: UIColor = UIColor.lightGray
-        var separatorColor: UIColor = UIColor.lightGray
-		var timeFont: UIFont = UIFont.systemFont(ofSize: 15)
-		var insets: UIEdgeInsets = UIEdgeInsets(top: 20, left: 15, bottom: 20, right: 15)
+	public class PosterConfig {
+		public var avaterHeight: CGFloat = 44
+        public var avatarPlaceholder: UIImage? = AEUIConfig.image(name: "user.png")
+		public var nameColor: UIColor = UIColor.darkGray
+		public var nameFont: UIFont = UIFont.systemFont(ofSize: 15)
+		public var timeTextColot: UIColor = UIColor.lightGray
+        public var separatorColor: UIColor = UIColor.lightGray
+		public var timeFont: UIFont = UIFont.systemFont(ofSize: 15)
+		public var insets: UIEdgeInsets = UIEdgeInsets(top: 20, left: 15, bottom: 20, right: 15)
 	}
 	
-	var headImageContentConfig = HeadImageContentConfig()
-	var separatorConfig = SeparatorConfig()
-	var textContentConfig = TextContentConfig()
-	var imageContentConfig = ImageContentConfig()
-	var titleConfig = TitleConfig()
-	var posterConfig = PosterConfig()
+	public var headImageContentConfig = HeadImageContentConfig()
+	public var separatorConfig = SeparatorConfig()
+	public var textContentConfig = TextContentConfig()
+	public var imageContentConfig = ImageContentConfig()
+	public var titleConfig = TitleConfig()
+	public var posterConfig = PosterConfig()
 }
